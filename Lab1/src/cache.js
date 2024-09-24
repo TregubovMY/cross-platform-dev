a540c40cdc4797d3f3f30b29f28fbf4ed403340f
Tregubov Maksim <tregubov-m@inbox.ru>
2024-09-24 20:37:28 +0300

    solution for lab 1

--- Lab1/src/cache.js
+++ Lab1/src/cache.js
@@ -1,3 +1,62 @@
 class Cache{
+  constructor(data) {
+    this.__data = this.normalizeData(data);
+    this.__log = "";
+  }
+
+  normalizeData(data){
+    return Object.keys(data).reduce((acc, key) => {
+      acc[key] = {
+        value: data[key].value || null,
+        quantity: data[key].value ? data[key].quantity || 1 : 0
+      };
+      
+      return acc;
+    }, {});
+  }
+
+  get log(){
+    return this.__log.trim();
+  }
+
+  value(key){
+    this.add2Log(key);
+
+    if(this.__data[key].quantity > 1){
+      this.decreaseQuantity(key);
+    }
+    else{
+      this.nullify(key);
+    }
+
+    return this.__data[key].value;
+  }
+
+  setQuantity(key, quantity){
+    if(key in this.__data && this.__data[key].value !== null){
+      this.__data[key].quantity = quantity;
+    } else {
+      throw new Error('Key not found in cache or value is null');
+    }
+  }
+
+  add(data){
+    this.__data = {...this.__data, ...this.normalizeData(data)};
+  }
+
+  private
+
+  nullify(key){
+    this.__data[key].value = null;
+    this.__data[key].quantity = 0;
+  }
+
+  decreaseQuantity(key) {
+    this.__data[key].quantity -= 1;
+  }
+
+  add2Log(key){
+    this.__log += `${key}: ${this.__data[key].value}, quantity: ${this.__data[key].quantity}\n`
+  }
 }
 export {Cache}
--- Lab1/test/cache.test.js
+++ Lab1/test/cache.test.js
@@ -1,5 +1,96 @@
 import {Cache} from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
+const data = {
+  'key1': {
+    value: 'some value',
+    quantity: 2 
+  },
+  'key2': {
+    value: 'some',
+    quantity: 1
+  },
+  'key3': {
+    value: 'some',
+  },
+  'key4': {}
+}
+
+let cache;
+
+beforeEach(() => {
+  cache = new Cache(data);
+})
+
+afterEach(() => {
+  cache = null
+})
+
+describe('Cache', () => {
+  describe('value', () => {
+    test('get value', () => {
+      expect(cache.value('key1')).toEqual(data['key1'].value)
+    })
+
+    test('quantity decreasing after request', () => {
+      cache.value('key2')
+      expect(cache.value('key2')).toEqual(null)
+    })
+
+    test('returns null when the quantity is not set', () => {
+      cache.value('key3');
+      expect(cache.value('key3')).toEqual(null);
+    });
+
+    test('returns null when the key is not found', () => {
+      expect(cache.value('key4')).toEqual(null);
+    });
+  });
+
+  describe('log', () => {
+    test('returns an empty string when no logs have been made', () => {
+      expect(cache.log).toEqual('');
+    });
+
+    test('returns the correct log entries', () => {
+      cache.value('key1');
+      cache.value('key2');
+
+      expect(cache.log).toEqual(
+        "key1: some value, quantity: 2\nkey2: some, quantity: 1"
+      );
+    });
+  });
+
+  describe('setQuantity', () => {
+    it('should set quantity for existing key', () => {
+      cache.setQuantity('key1', 5);
+      expect(cache.__data.key1.quantity).toBe(5);
+    });
+
+    it('should throw error for non-existent key', () => {
+      expect(() => cache.setQuantity('key99', 5)).toThrowError('Key not found in cache or value is null');
+    });
+
+    it('should throw error for key with null value', () => {
+      expect(() => cache.setQuantity('key5', 5)).toThrowError('Key not found in cache or value is null');
+    });
+  });
+
+  describe('add', () => {
+    it('should add new key-value pair', () => {
+      cache.add({ key8: { value: 'value4', quantity: 1 } });
+      expect(cache.__data.key8).toEqual({ value: 'value4', quantity: 1 });
+    });
+
+    it('should merge existing key-value pair', () => {
+      cache.add({ key6: { value: 'value5', quantity: 1 } });
+      cache.add({ key6: { value: 'newValue5', quantity: 2 } });
+      expect(cache.__data.key6).toEqual({ value: 'newValue5', quantity: 2 });
+    });
+
+    it('should normalize data before adding', () => {
+      cache.add({ key6: { value: 'value6' } });
+      expect(cache.__data.key6).toEqual({ value: 'value6', quantity: 1 });
+    });
+  });
 });
